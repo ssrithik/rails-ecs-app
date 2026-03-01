4b0e35c91c671fc86b777222d94988b7e2b3a24c
ssrithik <166838233+ssrithik@users.noreply.github.com>
2026-03-01 23:33:56 +0530

    buildspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-   root to: proc { [200, {}, ["Rails ECS App Running 🚀"]] }
+   root to: proc { [200, {}, ["Rails ECS App Running 🚀 CI/CD"]] }
   get "/health", to: proc { [200, {}, ["OK"]] }
 end
