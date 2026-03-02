ba7de8a39b05aedd2109ad3dbf0379b0572e463b
ssrithik <166838233+ssrithik@users.noreply.github.com>
2026-03-02 15:28:01 +0530

    configur

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-   root to: proc { [200, {}, ["Rails ECS App Running 🚀 CI/CD come on da I'm winning"]] }
+   root to: proc { [200, {}, ["Rails ECS App Running 🚀 CI/CD connection over"]] }
   get "/health", to: proc { [200, {}, ["OK"]] }
 end
